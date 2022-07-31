170f215d6d9fbf24ee64ae17d27af6200d812578
Priyank Thakkar <priyank@liven.com.au>
2022-08-01 01:26:52 +1000

    Added wait for db command along with unit tests

--- app/app/tests.py
+++ app/app/tests.py
@@ -3,15 +3,15 @@ Sample Test
 """
 
 from django.test import SimpleTestCase
-
 from app import calc
+
 
 class CalcTest(SimpleTestCase):
 
     def test_add_numbers(self):
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        res = calc.subtract(10,15)
-        self.assertEqual(res,-5)
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, -5)
